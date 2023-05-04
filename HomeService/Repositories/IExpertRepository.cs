9d118519d71e5fa8d6e0827c0d7c7602d6c4a39f
maryamLashgari <maryam.lashgarii@gmail.com>
2023-05-04 18:19:38 +0430

    add IexpertRepo

--- HomeService/Repositories/IExpertRepository.cs
+++ HomeService/Repositories/IExpertRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace HomeService.Repositories
+﻿using HomeService.Models.Entities;
+
+namespace HomeService.Repositories
 {
     public interface IExpertRepository
     {
+        void Create(Expert expert);
+        List<Expert> GetCustomers();
+        Customer GetById(int id);
+        void Update(Expert expert);
+        void DeleteById(int id, int deletedByUserId);
     }
 }
